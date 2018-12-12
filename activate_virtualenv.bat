99f1125b449a874e1868c695d36afbed950a933d
VgTajdd <aduranddiaz@gmail.com>
2018-12-12 12:22:48 +0000

    Scritps to run demo.py First demo using pygame, rectangle and time elapsed.

---
+++ activate_virtualenv.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+set CURRENT_DIR=%~dp0
+
+:: cd /d changes directly from C to without double command : "cd d:/some/route + d:"
+::cmd /k "cd /d %CURRENT_DIR%\venv\Scripts & activate & cd /d %CURRENT_DIR% & python test.py & deactivate"
+
+:: This make the same than the line above.
+call "%CURRENT_DIR%/../../venv/Scripts/activate.bat"
+call pip list
+call pip install -r requirements.txt
+
+cmd /k 
+
+call pip freeze > requirements.txt
+call deactivate
+endlocal
