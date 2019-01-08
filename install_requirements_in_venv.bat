aac230d18769e6546f033dc227ee24d7c1594a50
VgTajdd <aduranddiaz@gmail.com>
2019-01-08 15:54:42 +0000

    Added box2d-py as requirement. Added necessary SWIG_PATH to install_requirements_in_venv.bat to install box2d-py.

--- install_requirements_in_venv.bat
+++ install_requirements_in_venv.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
 set CURRENT_DIR=%~dp0
+set SWIG_PATH=C:/swigwin-3.0.12
+set PATH=%SWIG_PATH%;%PATH%;
 call "%CURRENT_DIR%/../../venv/Scripts/activate.bat"
 call pip install -r requirements.txt
 ::call pip list
